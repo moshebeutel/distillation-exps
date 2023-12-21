61b5a34b6053029a63bdf92c1264a4bb779c0211
Moshe Beutel <moshebeutel@gmail.com>
2023-12-21 15:16:06 +0200

    First debug run working

--- ddistexps/baseline/main.py
+++ ddistexps/baseline/main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import ray
 import os
 import time
@@ -10,6 +11,9 @@ from ddist.data import get_dataset
 from ddist.utils import spec_to_prodspace, dict_to_namespace, namespace_to_dict
 
 from ddistexps.baseline.trainer import BaselineTrainer as BLTrainer
+
+os.environ['TORCH_DATA_DIR'] = f'{Path.home()}/datasets/'
+os.environ['DDIST_EXPS_DIR'] = f'{Path.home()}/GIT/distillation-exps'
 from ddistexps.baseline.expcfg import EXPERIMENTS
 from ddistexps.baseline.expcfg import get_candgen
 from ddistexps.utils import get_dataflow
@@ -19,7 +23,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     msg = "The exeriment name as expcfg.EXPERIMENTS."
     parser.add_argument("--expname", type=str, help=msg, required=True,
-                        default=None)
+                        default='debug')
 
     expname = parser.parse_args().expname
     spec = EXPERIMENTS[expname]
@@ -39,9 +43,10 @@ if __name__ == '__main__':
     # Set environment variable RAY_ADDRESS to use a pre-existing cluster
     dfnamespace = 'DataFlow'
     ray.init(namespace=dfnamespace)
-    tracking_uri = os.environ['MLFLOW_TRACKING_URI']
-    mlflow.set_tracking_uri(tracking_uri)
-    rr("[green bold] Connecting to mlflow using:", tracking_uri)
+    # tracking_uri = os.environ['MLFLOW_TRACKING_URI']
+    # mlflow.set_tracking_uri(tracking_uri)
+    # rr("[green bold] Connecting to mlflow using:", tracking_uri)
+    mlflow.autolog()
 
     dflow = prod_space[0]['dataflow']
     dfctrl = ray.get(get_dataflow.remote(dflow, meta.worker_cfg.world_size))
