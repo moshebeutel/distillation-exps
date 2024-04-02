11f89320e82db81d1180f7b53948bd433fde9ffb
Ariel Solomon <ariel.solomon@gmail.com>
2024-04-02 14:00:20 +0300

    Update get_acc_val.py

--- doubledistill/ddistexps/ftdistil/get_acc_val.py
+++ doubledistill/ddistexps/ftdistil/get_acc_val.py
@@ -33,19 +33,31 @@ val_transforms_list = [
     'partial(transforms.functional.pad, padding=4)']
 
 # created dictionary for each ran model
-
+temp_val = 4.0
 for fold in folds:
     if 'resnetdebug' in fold:
         pts = os.listdir(fold)
         index = fold.split('_ind')[-1]
-        cand = fold.split('student_')[1][0]
         shift = [transformed for transformed in shifts_names if transformed in val_transforms_list[int(index)]][0]
-        val_acc = float(max([acc.split('-')[4].split('_')[:3][-1] for acc in pts]))
-        val_acc = "{:.1f}".format(val_acc)
-        t_acc = float(max([acc.split('-')[5].split('_')[2] for acc in pts]))
-        t_acc = "{:.1f}".format(t_acc)
-        temperature = float(max([acc.split('-')[5].split('_')[4][:-3] for acc in pts]))
-        temperature = "{:.2f}".format(temperature)
+        cand = fold.split('student_')[1][0]
+        #temperature = float(max([acc.split('-')[5].split('_')[4][:-3] for acc in pts]))
+        temperature = [acc.split('-')[5].split('_')[4][:-3] for acc in pts]
+        indices = [ind for ind, value in enumerate(temperature) if value == str(temp_val)]
+        #temperature = "{:.2f}".format(temperature)
+        if indices == []:
+            val_acc = 5.0
+            t_acc = 80.0
+        else:
+
+            temperature = temp_val
+            selected_pts = [pts[i] for i in indices]
+
+            val_acc = float(max([acc.split('-')[4].split('_')[:3][-1] for acc in selected_pts]))
+
+            val_acc = "{:.1f}".format(val_acc)
+            t_acc = float(max([acc.split('-')[5].split('_')[2] for acc in selected_pts]))
+            t_acc = "{:.1f}".format(t_acc)
+
         resnetdebug['shifts'].append(shift)
         resnetdebug['val_acc'].append(val_acc)
         resnetdebug['t_acc'].append(t_acc)
@@ -54,14 +66,26 @@ for fold in folds:
     elif 'resnetsmall' in fold:
         pts = os.listdir(fold)
         index = fold.split('_ind')[-1]
-        cand = fold.split('student_')[1][0]
         shift = [transformed for transformed in shifts_names if transformed in val_transforms_list[int(index)]][0]
-        val_acc = float(max([acc.split('-')[4].split('_')[:3][-1] for acc in pts]))
-        val_acc = "{:.1f}".format(val_acc)
-        t_acc = float(max([acc.split('-')[5].split('_')[2] for acc in pts]))
-        t_acc = "{:.1f}".format(t_acc)
-        temperature = float(max([acc.split('-')[5].split('_')[4][:-3] for acc in pts]))
-        temperature = "{:.2f}".format(temperature)
+        cand = fold.split('student_')[1][0]
+        #temperature = float(max([acc.split('-')[5].split('_')[4][:-3] for acc in pts]))
+        temperature = [acc.split('-')[5].split('_')[4][:-3] for acc in pts]
+        indices = [ind for ind, value in enumerate(temperature) if value == str(temp_val)]
+        #temperature = "{:.2f}".format(temperature)
+        if indices == []:
+            val_acc = 5.0
+            t_acc = 80.0
+        else:
+
+            temperature = temp_val
+            selected_pts = [pts[i] for i in indices]
+
+            val_acc = float(max([acc.split('-')[4].split('_')[:3][-1] for acc in selected_pts]))
+
+            val_acc = "{:.1f}".format(val_acc)
+            t_acc = float(max([acc.split('-')[5].split('_')[2] for acc in selected_pts]))
+            t_acc = "{:.1f}".format(t_acc)
+
         resnetsmall['shifts'].append(shift)
         resnetsmall['val_acc'].append(val_acc)
         resnetsmall['t_acc'].append(t_acc)
@@ -70,14 +94,26 @@ for fold in folds:
     else:
         pts = os.listdir(fold)
         index = fold.split('_ind')[-1]
-        cand = fold.split('student_')[1][0]
         shift = [transformed for transformed in shifts_names if transformed in val_transforms_list[int(index)]][0]
-        val_acc = float(max([acc.split('-')[4].split('_')[:3][-1] for acc in pts]))
-        val_acc = "{:.1f}".format(val_acc)
-        t_acc = float(max([acc.split('-')[5].split('_')[2] for acc in pts]))
-        t_acc = "{:.1f}".format(t_acc)
-        temperature = float(max([acc.split('-')[5].split('_')[4][:-3] for acc in pts]))
-        temperature = "{:.2f}".format(temperature)
+        cand = fold.split('student_')[1][0]
+        #temperature = float(max([acc.split('-')[5].split('_')[4][:-3] for acc in pts]))
+        temperature = [acc.split('-')[5].split('_')[4][:-3] for acc in pts]
+        indices = [ind for ind, value in enumerate(temperature) if value == str(temp_val)]
+        #temperature = "{:.2f}".format(temperature)
+        if indices == []:
+            val_acc = 5.0
+            t_acc = 80.0
+        else:
+
+            temperature = temp_val
+            selected_pts = [pts[i] for i in indices]
+
+            val_acc = float(max([acc.split('-')[4].split('_')[:3][-1] for acc in selected_pts]))
+
+            val_acc = "{:.1f}".format(val_acc)
+            t_acc = float(max([acc.split('-')[5].split('_')[2] for acc in selected_pts]))
+            t_acc = "{:.1f}".format(t_acc)
+
         resnetlarge['shifts'].append(shift)
         resnetlarge['val_acc'].append(val_acc)
         resnetlarge['t_acc'].append(t_acc)
@@ -85,28 +121,32 @@ for fold in folds:
         resnetlarge['cand'].append(cand)
 
 temp_range = [1,2,4]
-def plot_fig(resnetdebug,resnetsmall, resnetlarge, exp_name, temp_range):
+def plot_fig(resnetdebug,resnetsmall, resnetlarge, exp_name):#, temp_range):
 
 
     plt.figure(figsize=(10, 6))  # Set the figure size
-    plt.plot([float(num) for num in resnetsmall['val_acc']],marker ='.', markersize=6,color='b',label='resnetsmall-student_val')
-    plt.plot([float(num) for num in resnetdebug['val_acc']],marker ='.',markersize=6, color='r',label='resnetdebug-student_val')
-    plt.plot([float(num) for num in resnetlarge['val_acc']],marker ='.',markersize=6, color='g',label='resnetlarge-student_val')
-    plt.plot([float(num) for num in resnetsmall['t_acc']], marker='.', markersize=6, color='black', label='teacher-clip_val')
-    plt.legend()
+    plt.plot([float(num) for num in resnetsmall['val_acc']],marker ='.', markersize=9,color='b',label='resnetsmall-student_val',
+             linewidth=0)
+    plt.plot([float(num) for num in resnetdebug['val_acc']],marker ='.',markersize=9, color='r',label='resnetdebug-student_val',
+             linewidth=0)
+    plt.plot([float(num) for num in resnetlarge['val_acc']],marker ='.',markersize=9, color='g',label='resnetlarge-student_val',
+             linewidth=0)
+    plt.plot([float(num) for num in resnetsmall['t_acc']], marker='.', markersize=9, color='black', label='teacher-clip_val',
+             linewidth=0)
+    plt.legend(loc='upper right')
     # Set x-axis labels and rotation for better readability
     plt.xticks(range(len(resnetsmall['shifts'])), resnetsmall['shifts'], rotation=90)  # Rotate x-axis labels for long strings
 
     # Set axis labels and title
-    plt.xlabel("Shifts")
+    plt.xlabel("Shifts", fontsize=6)
     plt.ylabel("val_acc")
-    plt.ylim(0, 100)
-    plt.yticks(range(0, 101, 10))
+    plt.ylim(5, 85)
+    plt.yticks(range(5, 86, 5))
     plt.grid(True)  # Add grid lines for better visualization
     plt.title(f'Shifts after {exp_name}')
     plt.tight_layout()  # Adjust spacing between elements
     plt.savefig('accuracies_'+exp_name+'.png')
     plt.show()
 #use the dictionaries to plot accucaries
-exp_name = '25_epochs_temperature_range_1_4'
+exp_name = '25_epochs_temperature_range_1_4_'+str(temp_val)
 plot_fig(resnetdebug,resnetsmall, resnetlarge,exp_name)
